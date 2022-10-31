21542bb4bb3fe9cf35f39e86eb62b4bc6930d923
Narolex <felix.cordero.bcn@gmail.com>
2022-10-31 13:32:42 +0100

    Primer Commit

---
+++ mercado/pipelines.py
@@ -0,0 +1,52 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+#from itemadapter import ItemAdapter
+
+
+#class ComparativaInmueblesPipeline:
+    #def process_item(self, item, spider):
+        #return item
+
+import scrapy
+from scrapy import signals
+from scrapy.exporters import CsvItemExporter
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exceptions import DropItem
+from scrapy import Request
+import csv
+
+
+
+class MercadoPipeline(objet):
+    def __init__(self):
+        self.files = {}
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        pipeline = cls()
+        crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
+        crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
+        return pipeline
+
+    def spider_opened(self, spider):
+        file = open('%s_items.csv'% spider.name,'w+b')
+        self.files[spider] = file
+        self.exporter = CsvItemExporter(file)
+        self.exporter.fields_to_export = ['titulo','voltaje','precio','condicion','envio','stock','opiniones',
+        'ventas_producto','vendedor_url','tipo_vendedor','reputacion','ventas_vendedor']
+        self.exporter.start_exporting()
+
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        file = self.files.pop(spider)
+        file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
